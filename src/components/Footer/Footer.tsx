d8bfc21d987eb7d4aefbfdfe088d7695bf8735b3
sdavidson90 <s.davidson90@hotmail.ca>
2020-07-02 14:03:14 -0700

    git extension practice

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <div>
+    <div className="footer">
+      <h1>placeholder</h1>
+      <h1>placeholder</h1>
+      <h1>placeholder</h1>
+      <h1>placeholder</h1>
       <h1>placeholder</h1>
     </div>
   );
